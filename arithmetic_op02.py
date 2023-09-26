391a0b6a4eff8ad704a882efe863321af02fbe66
DoniyorSayfiddinov <sayfiddinovdoniyor833@gmail.com>
2023-09-26 08:26:41 +0500

    2-ishlandi

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,5 +1,8 @@
 #Create a varaible 'number' and assign it the value of 43.
-
+number=43
 #Create a variable 'answer' and assign it the sum of the number's digits.
-
+a=4
+#Create a variable 'answer' and assign it the sum of the number's digits.)
+b=3
 #Print the vallue of the 'answer'.
+print(a+b)
